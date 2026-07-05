e8059193db34c9910464df37bb36fb9d5c00a09c
Tauke190 <avinashgyavali190@gmail.com>
2026-07-05 02:17:56 -0400

    Making RLT finetuning work and added videos for training

--- videoxlpro/scripts/rlt_finetune.sh
+++ videoxlpro/scripts/rlt_finetune.sh
@@ -19,11 +19,13 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --use_rlt True \
     --model_name_or_path ${MODEL_PATH} \
     --version ${PROMPT_VERSION} \
-    --data_path /home/av354855/EfficientVideoXLPro/llava_v1_5_mix665k_img_only_fifth.json \
+    --data_path /home/av354855/EfficientVideoXLPro/data_mix.yaml \
     --image_folder /home/c3-0/datasets/llava_665K/playground/data \
-    --video_folder /home/c3-0/datasets/llava_665K/playground/data \
+    --video_folder /home/c3-0/datasets/Ego4D/videos/h264 \
     --mm_tunable_parts="mm_vision_tower,mm_mlp_adapter,mm_language_model" \
     --mm_vision_tower_lr=2e-6 \
+    --frames_upbound 32 \
+    --video_fps 1 \
     --vision_tower ${VISION_MODEL_VERSION} \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -1 \
